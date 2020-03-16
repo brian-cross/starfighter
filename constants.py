7e75c38e3aa38a84ab1933255ab096a128e7455c
TuxFan77 <briancross@shaw.ca>
2020-03-15 22:05:43 -0600

    Added enemy ship sprites to assets. Changed the assets directory structure.

--- constants.py
+++ constants.py
@@ -13,4 +13,6 @@ PLAYER_SHIP_DRAG = 0.1
 
 # File paths
 IMAGES_DIR = 'assets/images'
-PLAYER_SHIP_FILENAME = f'{IMAGES_DIR}/player_ship.png'
+PLAYER_SPRITES_DIR = f'{IMAGES_DIR}/player_sprites'
+ENEMY_SPRITES_DIR = f'{IMAGES_DIR}/enemy_sprites'
+PLAYER_SHIP_FILENAME = f'{PLAYER_SPRITES_DIR}/player_ship.png'
